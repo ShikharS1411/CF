fa7ff2e440fd0332a8c56b094e7225bc9d5cf685
Shikhar <shikharsharma94147023@gmail.com>
2023-08-19 18:16:21 +0530

    CF template

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define lld long long double
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define fi first
+#define se second
+const ll mod = 1e9 + 7;
+// (a+b)%mod=((a%mod)+(b%mod))%mod
+// (a-b)%mod=((a%mod)-(b%mod)+mod)%mod
+// (a*b)%mod=((a%mod)*(b%mod))%mod
+// '0'=48,a=97,A=65
+void solve(){
+    
+}
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        solve();
+        // cout<<endl;
+    }
+    return 0;
+}
