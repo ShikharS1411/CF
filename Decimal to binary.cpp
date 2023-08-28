ec2b837dd2768d64dc455acd5f40e6d06fc6005e
Shikhar <shikharsharma94147023@gmail.com>
2023-08-28 16:55:35 +0530

    cf

--- Decimal to binary.cpp
+++ Decimal to binary.cpp
@@ -10,3 +10,10 @@ while(val){
 reverse(bits.begin(),bits.end());
 for(auto x:bits)cout<<x<<" ";
 cout<<endl;
+
+
+/*
+. for every binary num 1100 the decimal no till its set bit is always a divisor of it,
+  eg 1100(12) take 100(4) which is a divisor
+  the above concept is useful for qs in which divide by divisor is asked and stuff 
+*/
