d09613fb904f69e8f94865c932ff8076bd3caf8f
Shikhar <shikharsharma94147023@gmail.com>
2023-09-04 01:11:38 +0530

    cf

--- apply math not sliding window.cpp
+++ apply math not sliding window.cpp
@@ -1,5 +1,8 @@
 //in qs when sw is to be moved and also a certain position index in it as well just apply math
 // ->int val=ind%k;(index=original position of that element) and then just iterate all over the loop by just(i++);
 //as only unique values (val) we'll get in every sw wit iteration +=k; ez
+//this appraoch wil always be optimal as well (for greedy problems)
 
-//but in 2d matrix this ind position would be determined by that particular i,j which would be given in q 
+//but in 2d matrix this ind position would be determined by that particular i,j which would be given in q
+// eg->https://codeforces.com/contest/1717/problem/B
+//sol->https://codeforces.com/contest/1717/submission/221608547 
