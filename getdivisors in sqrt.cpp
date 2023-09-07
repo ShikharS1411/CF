92317bea315ce3359123585dde85ef55b199b362
Shikhar <shikharsharma94147023@gmail.com>
2023-09-08 00:28:37 +0530

    cf

--- getdivisors in sqrt.cpp
+++ getdivisors in sqrt.cpp
@@ -1,7 +1,7 @@
 void getdiv(int n,vector<int>&a){
 	for (int i=1;i<=sqrt(n);i++){
 		if(n%i==0){
-			if(n/i==i)a.pb(i)
+			if(n/i==i)a.pb(i);
             else{
                 a.pb(i);
                 a.pb(n/i);
