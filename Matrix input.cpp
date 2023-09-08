0315c03ce2edd4132989a192be28b91b8c863962
Shikhar <shikharsharma94147023@gmail.com>
2023-09-08 17:19:46 +0530

    cf

---
+++ Matrix input.cpp
@@ -0,0 +1,10 @@
+// 1. push_back method(for arrays/int*)->useful when col size is variable
+    ll n;cin>>n;
+    vector<vector<ll>>a(n);
+    for(ll i=0;i<n;i++){
+        ll k;cin>>k;//col size
+        for(ll j=0;j<k;j++){
+            ll x;cin>>x;
+            a[i].pb(x);
+        }
+    }
