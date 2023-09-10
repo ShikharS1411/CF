f8f6a56c3e1efa39d4d4ff068e85eddb054616b7
Shikhar <shikharsharma94147023@gmail.com>
2023-09-11 01:22:02 +0530

    cf

--- Matrix input.cpp
+++ Matrix input.cpp
@@ -5,6 +5,18 @@
         ll k;cin>>k;//col size
         for(ll j=0;j<k;j++){
             ll x;cin>>x;
-            a[i].pb(x);//cant push vectore
+            a[i].pb(x);//cant push vectore if size is predefined
         }
+    }
+    //when size is not defined
+    ll n;cin>>n;
+    vector<vector<ll>>a;
+    for(ll i=0;i<n;i++){
+        ll k;cin>>k;//col size
+        vector<int>temp;
+        for(ll j=0;j<k;j++){
+            ll x;cin>>x;
+            temp.pb(x);
+        }
+        a[i].pb(temp);
     }
