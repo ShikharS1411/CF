809d690cc3eba4e5933c99cc2fb01b2f2e9f5d38
Shikhar <shikharsharma94147023@gmail.com>
2024-01-02 02:17:37 +0530

    template

--- template.cpp
+++ template.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-using namespace __gnu_pbds;  
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>
 using namespace std;
 #define ll long long int
 #define lld long long double
@@ -12,13 +12,13 @@ using namespace std;
 #define ppb pop_back
 #define fi first
 #define se second
-const ll mod = 1e9 + 7;
-// (a+b)%mod=((a%mod)+(b%mod))%mod
-// (a-b)%mod=((a%mod)-(b%mod)+mod)%mod
-// (a*b)%mod=((a%mod)*(b%mod))%mod
+const ll mod=1e9+7;
+ll modadd(ll a,ll b){return ((a%mod)+(b%mod))%mod;}
+ll modsub(ll a,ll b){return ((a%mod)-(b%mod)+mod)%mod;}
+ll modmul(ll a,ll b){return ((a%mod)*(b%mod))%mod;}
 // '0'=48,a=97,A=65
 void solve(){
-
+    
 }
 int main(){
     ios_base::sync_with_stdio(false);
