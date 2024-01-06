e3ff29d2da06b733f1ffe99796a4c17018843644
Shikhar <shikharsharma94147023@gmail.com>
2024-01-06 19:06:35 +0530

    cf

---
+++ Go with Mid element.cpp
@@ -0,0 +1,3 @@
+//qs in which we have to find min cost to rearrange/collect stuff in some pos->always go with mid element in that case
+https://codeforces.com/contest/1520/problem/E
+//also whenever need to reduce two no(s) such that they become equall after any no of operations, try to make them equall to 1
